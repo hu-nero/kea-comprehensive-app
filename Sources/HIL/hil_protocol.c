704f78215b7f0a900158c786cc84abbc846912a6
hu-nero <894534866@qq.com>
2023-07-28 14:33:13 +0800

    均衡清空；单体、温度、均衡的编号改为16bit；单体单位改为0.0001V；均衡改为先设置全体，在判断设置单体;按协议转换HV1，HV2，HV3和无转换的绝缘值

--- Sources/HIL/hil_protocol.c
+++ Sources/HIL/hil_protocol.c
@@ -17,6 +17,11 @@ volatile static uint8_t HIL_KPE_SET_Transmit = 0;
 volatile static uint8_t HIL_KPL_SET_Transmit = 0;
 volatile static uint8_t HIL_BAL_SET_Transmit = 0;
 
+typedef union
+{
+    uint16_t u16Tmp;
+    uint8_t u8Tmp[2];
+} UNION_8TO16_Struct;
 
 void
 HIL_protocol_handle(const uint32_t can_id, uint8_t *can_data)
@@ -27,6 +32,7 @@ HIL_protocol_handle(const uint32_t can_id, uint8_t *can_data)
     uint16_t V_HeatVOL_CAN_Cache = 0;
     uint32_t Err_OpenTemp = 0;
     uint32_t VOL_OpenCheck = 0;
+    UNION_8TO16_Struct unTmp;
     switch (can_id)
     {
         case HIL_ID_KPE_1:
@@ -41,8 +47,7 @@ HIL_protocol_handle(const uint32_t can_id, uint8_t *can_data)
                     Current_CAN_Cache = (uint32_t)i32Tmp;
 
                     Current = (int16_t)(i32Tmp);
-                    //V_HV1 = V_HV1_CAN_Cache*15;
-                    V_HV1 = V_HV1_CAN_Cache;
+                    V_HV1 = V_HV1_CAN_Cache*15;
 
                 } else {
                     HIL_KPE_SET_Flag = 0;
@@ -56,6 +61,7 @@ HIL_protocol_handle(const uint32_t can_id, uint8_t *can_data)
                 } else {
                     HIL_KPE_SET_Transmit = 0;
                 }
+                CAN_TranData(&V_HV1,0x30,2);
             }
             break;
         case HIL_ID_KPE_2:
@@ -63,14 +69,14 @@ HIL_protocol_handle(const uint32_t can_id, uint8_t *can_data)
                 if (HIL_KPE_SET_Flag == 1) {
                     V_HV2_CAN_Cache = (uint16_t)((((uint16_t)can_data[1])<<8)|((uint16_t)can_data[0]));
                     V_HeatVOL_CAN_Cache = (uint16_t)((((uint16_t)can_data[3])<<8)|((uint16_t)can_data[2]));
-                    //V_HV2 = V_HV2_CAN_Cache*15;
-                    //V_HeatVOL = V_HeatVOL_CAN_Cache*15;
-                    V_HV2 = V_HV2_CAN_Cache;
-                    V_HeatVOL = V_HeatVOL_CAN_Cache;
+                    V_HV2 = V_HV2_CAN_Cache*15;
+                    V_HeatVOL = V_HeatVOL_CAN_Cache*15;
                 } else {
                     V_HV2 = 0;
                     V_HeatVOL = 0;
                 }
+                CAN_TranData(&V_HV2,0x31,2);
+                CAN_TranData(&V_HeatVOL,0x32,2);
             }
             break;
         case HIL_ID_KPE_3:
@@ -82,24 +88,35 @@ HIL_protocol_handle(const uint32_t can_id, uint8_t *can_data)
                     R_HV2_To_GND = 0xFFFF;
                     R_FLQ_To_GND = 0xFFFF;
                 }
+                CAN_TranData(&R_FLQ_To_GND,0x33,2);
+                CAN_TranData(&R_HV2_To_GND,0x34,2);
             }
             break;
         case HIL_ID_KPL_1:
             {
                 if (can_data[0] == 0xAA)
                 {
+                    UNION_8TO16_Struct cell;
                     HIL_KPL_SET_Flag = 1;
-                    if (can_data[4] != 0)
+                    unTmp.u8Tmp[0] = can_data[4];
+                    unTmp.u8Tmp[1] = can_data[5];
+                    if (unTmp.u16Tmp != 0)
                     {
-                        if (can_data[4] <= _CV_CH_NUM)
+                        if (unTmp.u16Tmp <= _CV_CH_NUM)
                         {
-                            CellVoltage[can_data[4]-1] = (uint16_t)((((uint16_t)can_data[6])<<8)|((uint16_t)can_data[5]));
+                            cell.u8Tmp[0] = can_data[6];
+                            cell.u8Tmp[1] = can_data[7];
+                            cell.u16Tmp /= 10;
+                            CellVoltage[unTmp.u16Tmp-1] = cell.u16Tmp;
                         }
                     } else
                     {
                         for (uint8_t index = 0; index < _CV_CH_NUM; index ++)
                         {
-                            CellVoltage[index] = (uint16_t)((((uint16_t)can_data[3])<<8)|((uint16_t)can_data[2]));
+                            cell.u8Tmp[0] = can_data[2];
+                            cell.u8Tmp[1] = can_data[3];
+                            cell.u16Tmp /= 10;
+                            CellVoltage[index] = cell.u16Tmp;
                         }
                     }
                 } else
@@ -125,20 +142,24 @@ HIL_protocol_handle(const uint32_t can_id, uint8_t *can_data)
         case HIL_ID_KPL_2:
             {
                 if (HIL_KPL_SET_Flag == 1) {
-                    if (can_data[1] == 0) {
+                    unTmp.u8Tmp[0] = can_data[1];
+                    unTmp.u8Tmp[1] = can_data[2];
+                    if (unTmp.u16Tmp == 0) {
                         memset(NTCTemp, can_data[0], sizeof(NTCTemp));
                     } else {
-                        if (can_data[1] <= 40) {
-                            NTCTemp[can_data[1]-1] = can_data[2];
+                        if (unTmp.u16Tmp <= 40) {
+                            NTCTemp[unTmp.u16Tmp-1] = can_data[3];
                         }
                     }
-                    Err_OpenTemp = (uint32_t)can_data[3];
-                    VOL_OpenCheck = can_data[4];
+                    Err_OpenTemp = (uint32_t)can_data[4];
+                    VOL_OpenCheck = can_data[5];
                 } else {
                     memset(NTCTemp, 0, sizeof(NTCTemp));
                     Err_OpenTemp = 0;
                     VOL_OpenCheck = 0;
                 }
+                CAN_TranData(&NTCTemp[0],0x40,8);
+                CAN_TranData(&NTCTemp[8],0x41,8);
             }
             break;
         case HIL_ID_KPL_2_2:
@@ -151,18 +172,19 @@ HIL_protocol_handle(const uint32_t can_id, uint8_t *can_data)
                 {
                     HIL_BAL_SET_Flag = 1;
                     //clear balance cache after each SPI communication
-
-                    if (can_data[4] != 0)
-                    {
-                        if (can_data[4] <= _CV_CH_NUM)
-                        {
-                            ComBalanceEnergyCache[can_data[4]-1] = can_data[5];
-                        }
-                    } else
+                    unTmp.u8Tmp[0] = can_data[4];
+                    unTmp.u8Tmp[1] = can_data[5];
                     {
                         for(uint8_t i=0;i<_CV_CH_NUM;i++)
                         {
                             ComBalanceEnergyCache[i] = can_data[2];
+                        }
+                    }
+                    if (unTmp.u16Tmp != 0)
+                    {
+                        if (unTmp.u16Tmp <= _CV_CH_NUM)
+                        {
+                            ComBalanceEnergyCache[unTmp.u16Tmp-1] = can_data[6];
                         }
                     }
                 } else {
@@ -175,6 +197,8 @@ HIL_protocol_handle(const uint32_t can_id, uint8_t *can_data)
                 } else {
                     HIL_BAL_SET_Transmit = 0;
                 }
+                CAN_TranData(&ComBalanceEnergyCache[0],0x50,8);
+                CAN_TranData(&ComBalanceEnergyCache[8],0x51,8);
             }
             break;
         default:break;
